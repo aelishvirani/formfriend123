23ccd34963de71cee5ff53bfc1f8bf78ccb05e5a
Aelish <aelishvirani1711@gmail.com>
2023-03-30 10:34:13 +0530

    form.jsx modified

--- src/components/Tracking.jsx
+++ src/components/Tracking.jsx
@@ -1,51 +1,4 @@
-// import React, { useEffect, useState } from 'react';
-// import { useParams } from 'react-router-dom';
 
-// const Tracking = (props) => {
-//   const [dataGroup, setDataGroup] = useState([]);
-//   const { id } = useParams();
-
-//   const getData = async () => {
-//     try {
-//       const res = await fetch(`http://formfriend.cleverapps.io/api/form/EditForm/${id}`, {
-//         mode: 'cors',
-//         headers: {
-//           'Access-Control-Allow-Origin': '*',
-//           'Content-Type': 'application/json',
-//           Authorization: 'Bearer ' + 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiYUBnbWFpbC5jb20iLCJlbWFpbCI6ImFAZ21haWwuY29tIiwianRpIjoiNTI4NDdjNmMtMTc0Yi00ZjAzLTljOGEtYmJhZjlkYjBkNWUyIiwibmJmIjoxNjc5ODU2MzYzLCJleHAiOjE2ODIyNzU1NjMsImlhdCI6MTY3OTg1NjM2M30.lT1YLqsgk6vKUm_oO5wigvonyzAEutJphVTNyuR1Zu1bQ4hkIrSk4QgIwHGJcLVjCG42Ba0ykrGD8nvLVp4BtQ',
-//         },
-//       });
-//       const actualdata = await res.json();
-//       console.log(actualdata.group);
-//       setDataGroup(actualdata.group);
-//     } catch (err) {
-//       console.log('err');
-//     }
-//   };
-
-//   useEffect(() => {
-//     getData();
-//   }, []);
-
-//   return (
-//     <>
-//       <div>Tracking {id}</div>
-//       {dataGroup.map((value, index) => {
-//         return (
-//           <React.Fragment key={index}>
-//             <h1>
-//               {index + 1}. {value.groupName}
-//             </h1>
-//             <table className="table table-light table-hover" style={{ marginRight: '10px' }}>
-//               <thead>
-//                 <tr>
-//                   <th scope="col">#</th>
-//                   <th scope="col">Email</th>
-//                   <th scope="col">Seen</th>
-//                   <th scope="col">Filled</th>
-//                 </tr>
-//               </thead>
-//               <tbody>
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
@@ -65,42 +18,51 @@ const Tracking = (props) => {
         },
       });
       const actualdata = await res.json();
-      console.log(actualdata.group);
+      // console.log(actualdata.group);
       setDataGroup(actualdata.group);
     } catch (err) {
       console.log('err');
     }
   };
-
- 
+  
+  
   const handleFilterData = async () => {
     const filtered = {};
-    dataGroup.forEach((group, groupIndex) => {
+    dataGroup.forEach(async (group, groupIndex) => {
       filtered[groupIndex] = [];
       group.participants.forEach((participant) => {
         if ((participant.seen === true && participant.filled === false) || (participant.seen === false && participant.filled === false)) {
           filtered[groupIndex].push(participant.email);
         }
       });
-    });
-    setFilteredData(filtered);
-    console.log(filteredData);
-  
+      
+      const reminder={
+        "GroupId":`${group.groupId}`,
+        "Message":"Test",
+        "Participants":filtered[groupIndex],
+      }
+      console.log(group.groupId);
+      console.log(reminder);
     try {
-      const res = await fetch(`http://formfriend.cleverapps.io/api//form/AddReminder`, {
+      const res = await fetch(`http://formfriend.cleverapps.io/api/form/AddReminder`, {
         method: 'POST',
         mode: 'cors',
         headers: {
           'Content-Type': 'application/json',
           Authorization: 'Bearer ' + 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiYUBnbWFpbC5jb20iLCJlbWFpbCI6ImFAZ21haWwuY29tIiwianRpIjoiNTI4NDdjNmMtMTc0Yi00ZjAzLTljOGEtYmJhZjlkYjBkNWUyIiwibmJmIjoxNjc5ODU2MzYzLCJleHAiOjE2ODIyNzU1NjMsImlhdCI6MTY3OTg1NjM2M30.lT1YLqsgk6vKUm_oO5wigvonyzAEutJphVTNyuR1Zu1bQ4hkIrSk4QgIwHGJcLVjCG42Ba0ykrGD8nvLVp4BtQ',
         },
-        body: JSON.stringify(filtered),
+        body: JSON.stringify(reminder),
       });
       const data = await res.json();
       console.log(data);
     } catch (err) {
       console.log(err);
     }
+    });
+    setFilteredData(filtered);
+    console.log(filtered);
+    console.log(filteredData);
+  
   };
   
 
@@ -115,7 +77,7 @@ const Tracking = (props) => {
       </div>
       <button className="btn btn-primary me-md-2" type="button">Add Group</button>
      <div className="d-grid gap-2 d-md-flex justify-content-md-end">
-        <button className="btn btn-primary me-md-2" type="button" onClick={handleFilterData()}>Reminder</button>
+        <button className="btn btn-primary me-md-2" type="button" onClick={handleFilterData}>Reminder</button>
       </div>
       {dataGroup.map((value, index) => {
         return (
@@ -154,6 +116,50 @@ const Tracking = (props) => {
 };
 
 export default Tracking;
+
+// const [emails, setEmails] = useState({
+  //   seenYesFilledNo: [],
+  //   seenNoFilledNo: []
+  // });
+  // const handleReminder = () => {
+  //   const seenYesFilledNo = [];
+  //   const seenNoFilledNo = [];
+  //   dataGroup.forEach(group => {
+  //     group.participants.forEach(participant => {
+  //       if (participant.seen && !participant.filled) {
+  //         seenYesFilledNo.push(participant.email);
+  //       } else if (!participant.seen && !participant.filled) {
+  //         seenNoFilledNo.push(participant.email);
+  //       }
+  //     });
+  //   });
+  //   setEmails({ seenYesFilledNo, seenNoFilledNo });
+  //   console.log(emails);
+  //   addReminder();
+  // };
+  
+  // const addReminder = async () => {
+  //   try {
+  //     const res = await fetch('http://formfriend.cleverapps.io/api//form/AddReminder', {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //         Authorization: 'Bearer ' + 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiYUBnbWFpbC5jb20iLCJlbWFpbCI6ImFAZ21haWwuY29tIiwianRpIjoiNTI4NDdjNmMtMTc0Yi00ZjAzLTljOGEtYmJhZjlkYjBkNWUyIiwibmJmIjoxNjc5ODU2MzYzLCJleHAiOjE2ODIyNzU1NjMsImlhdCI6MTY3OTg1NjM2M30.lT1YLqsgk6vKUm_oO5wigvonyzAEutJphVTNyuR1Zu1bQ4hkIrSk4QgIwHGJcLVjCG42Ba0ykrGD8nvLVp4BtQ'
+  //       },
+  //       body: JSON.stringify({
+  //         groupIds: [id],
+  //         seenYesFilledNo: emails.seenYesFilledNo,
+  //         seenNoFilledNo: emails.seenNoFilledNo
+  //       })
+  //     });
+  //     const data = await res.json();
+  //     console.log(data);
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // };
+
+
 
  // const handleFilterData = () => {
   //   const filtered = {};
